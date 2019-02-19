52115842a9973ab0fe8b37bbc4ddcbe70a506ae3
vinfinit <artemenko.vladimi@gmail.com>
2019-02-19 13:56:51 +0300

    Updates

---
+++ precache-manifest.d35a5c965502c7be92fefcb3030e7882.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fafd17207814fc49ec1a",
+    "url": "/ConnectFour/static/js/runtime~main.fafd1720.js"
+  },
+  {
+    "revision": "1e99afc0744d046805ac",
+    "url": "/ConnectFour/static/js/main.1e99afc0.chunk.js"
+  },
+  {
+    "revision": "d1628d3e5e9d2fd2ee90",
+    "url": "/ConnectFour/static/js/2.d1628d3e.chunk.js"
+  },
+  {
+    "revision": "1e99afc0744d046805ac",
+    "url": "/ConnectFour/static/css/main.22ca7d94.chunk.css"
+  },
+  {
+    "revision": "1ce9133e1e8ea2bc3f2e9826f3fab5f9",
+    "url": "/ConnectFour/index.html"
+  }
+];
